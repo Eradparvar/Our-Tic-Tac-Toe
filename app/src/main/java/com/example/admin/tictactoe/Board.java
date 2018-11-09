8c05f30287e815c82cac626e09ef24985a81e7fc
Shlomo Gordon <sgordon16@student.touro.edu>
2018-11-08 23:44:25 -0500

    Update

--- app/src/main/java/com/example/admin/tictactoe/Board.java
+++ app/src/main/java/com/example/admin/tictactoe/Board.java
@@ -4,11 +4,11 @@ public class Board {
 
 	private State[] boardState = new State[9];
 	private State currentTurn;
-	
-	
+
+
 	//Constructor, sets the board's state to all blank:
 	public Board(State firstTurn){
-	    currentTurn = firstTurn;
+		currentTurn = firstTurn;
 		resetBoard();
 	}
 
@@ -38,9 +38,9 @@ public class Board {
 	//This method takes a position and returns the string representation of the enum: 
 	public String getString(int position){
 		if (boardState[position] == State.X){
-			return "x";
+			return "X";
 		} else if (boardState[position] == State.O){
-			return "o";
+			return "O";
 		} else {
 			return "";
 		}
@@ -60,347 +60,347 @@ public class Board {
 	 */
 	private boolean gameCompleted(State lastMove, int position) {
 		switch (position) {
-		case 0:
-			/*-
-			 * The following condition tests for a line on the top row:
-			 * 		-------------
-			 * 		| * | * | * |
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		|   |   |   |
-			 *		-------------
-			 */
-			if (boardState[1] == lastMove && boardState[2] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a line along the left side:
-			 * 		-------------
-			 *		| * |   |   |
-			 *		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 */
-			else if (boardState[3] == lastMove && boardState[6] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a diagonal line (top,left to bottom,right): 
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 */
-			else if (boardState[4] == lastMove && boardState[8] == lastMove) {
-				return true;
-			}
-			// In any other case the last move did not complete the game:
-			else
+			case 0:
+				/*-
+				 * The following condition tests for a line on the top row:
+				 * 		-------------
+				 * 		| * | * | * |
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		|   |   |   |
+				 *		-------------
+				 */
+				if (boardState[1] == lastMove && boardState[2] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a line along the left side:
+				 * 		-------------
+				 *		| * |   |   |
+				 *		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 */
+				else if (boardState[3] == lastMove && boardState[6] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a diagonal line (top,left to bottom,right):
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 */
+				else if (boardState[4] == lastMove && boardState[8] == lastMove) {
+					return true;
+				}
+				// In any other case the last move did not complete the game:
+				else
+					return false;
+			case 1:
+				/*-
+				 * The following condition tests for a line across the top:
+				 * 		-------------
+				 * 		| * | * | * |
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 */
+				if (boardState[0] == lastMove && boardState[2] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a line down the middle:
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 */
+				else if (boardState[4] == lastMove && boardState[7] == lastMove) {
+					return true;
+				} else
+					return false;
+			case 2:
+				/*-
+				 * The following condition tests for a line across the top:
+				 * 		-------------
+				 * 		| * | * | * |
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 */
+				if (boardState[0] == lastMove && boardState[1] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a line down the right side:
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 */
+				else if (boardState[5] == lastMove && boardState[8] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a diagonal (top,right to bottom,left):
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 */
+				else if (boardState[4] == lastMove && boardState[6] == lastMove) {
+					return true;
+				}
+			case 3:
+				/*-
+				 * The following condition tests for a line down the left:
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 */
+				if (boardState[0] == lastMove && boardState[6] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a line across the middle:
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		| * | * | * |
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 */
+				else if (boardState[4] == lastMove && boardState[5] == lastMove) {
+					return true;
+				} else
+					return false;
+			case 4:
+				/*-
+				 * The following condition tests for a line down the center:
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 */
+				if (boardState[1] == lastMove && boardState[7] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a line across the middle:
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		| * | * | * |
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 */
+				else if (boardState[3] == lastMove && boardState[5] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a diagonal (top,left to bottom,right):
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 */
+				else if (boardState[0] == lastMove && boardState[8] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a diagonal (top,right to bottom,left):
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 */
+				else if (boardState[2] == lastMove && boardState[6] == lastMove) {
+					return true;
+				}
+			case 5:
+				/*-
+				 * The following condition tests for a line down the right side:
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 */
+				if (boardState[2] == lastMove && boardState[8] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a line across the middle:
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		| * | * | * |
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 */
+				if (boardState[3] == lastMove && boardState[4] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a line down the right:
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 */
+				else if (boardState[2] == lastMove && boardState[8] == lastMove) {
+					return true;
+				}
+			case 6:
+				/*-
+				 * The following condition tests for a line down the left:
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 */
+				if (boardState[0] == lastMove && boardState[3] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a line across the bottom:
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		| * | * | * |
+				 * 		-------------
+				 */
+				else if (boardState[7] == lastMove && boardState[8] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a diagonal line (bottom,left to top,right):
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 */
+				else if (boardState[2] == lastMove && boardState[4] == lastMove) {
+					return true;
+				}
+			case 7:
+				/*-
+				 * The following condition tests for a line across the bottom:
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		| * | * | * |
+				 * 		-------------
+				 */
+				if (boardState[6] == lastMove && boardState[8] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a line down the middle:
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 */
+				else if (boardState[1] == lastMove && boardState[7] == lastMove) {
+					return true;
+				}
+			case 8:
+				/*-
+				 * The following condition tests for a line across the bottom:
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		|   |   |   |
+				 * 		-------------
+				 * 		| * | * | * |
+				 * 		-------------
+				 */
+				if (boardState[6] == lastMove && boardState[7] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a line down the right side:
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 */
+				else if (boardState[2] == lastMove && boardState[5] == lastMove) {
+					return true;
+				}
+				/*-
+				 * The following condition tests for a diagonal:
+				 * 		-------------
+				 * 		| * |   |   |
+				 * 		-------------
+				 * 		|   | * |   |
+				 * 		-------------
+				 * 		|   |   | * |
+				 * 		-------------
+				 */
+				else if (boardState[0] == lastMove && boardState[4] == lastMove) {
+					return true;
+				}
+			default:
 				return false;
-		case 1:
-			/*-
-			 * The following condition tests for a line across the top: 
-			 * 		-------------
-			 * 		| * | * | * |
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 */
-			if (boardState[0] == lastMove && boardState[2] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a line down the middle: 
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 */
-			else if (boardState[4] == lastMove && boardState[7] == lastMove) {
-				return true;
-			} else
-				return false;
-		case 2:
-			/*-
-			 * The following condition tests for a line across the top: 
-			 * 		-------------
-			 * 		| * | * | * |
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 */
-			if (boardState[0] == lastMove && boardState[1] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a line down the right side: 
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 */
-			else if (boardState[5] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a diagonal (top,right to bottom,left): 
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 */
-			else if (boardState[4] == lastMove && boardState[6] == lastMove) {
-				return true;
-			}
-		case 3:
-			/*-
-			 * The following condition tests for a line down the left: 
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 */
-			if (boardState[0] == lastMove && boardState[6] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a line across the middle:
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		| * | * | * |
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 */
-			else if (boardState[4] == lastMove && boardState[5] == lastMove) {
-				return true;
-			} else
-				return false;
-		case 4:
-			/*-
-			 * The following condition tests for a line down the center:
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 */
-			if (boardState[1] == lastMove && boardState[7] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a line across the middle:
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		| * | * | * |
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 */
-			else if (boardState[3] == lastMove && boardState[5] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a diagonal (top,left to bottom,right):
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 */
-			else if (boardState[0] == lastMove && boardState[8] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a diagonal (top,right to bottom,left):
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 */
-			else if (boardState[2] == lastMove && boardState[6] == lastMove) {
-				return true;
-			}
-		case 5:
-			/*-
-			 * The following condition tests for a line down the right side:
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 */
-			if (boardState[2] == lastMove && boardState[8] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a line across the middle:
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		| * | * | * |
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 */
-			if (boardState[3] == lastMove && boardState[4] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a line down the right:
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 */
-			else if (boardState[2] == lastMove && boardState[8] == lastMove) {
-				return true;
-			}
-		case 6:
-			/*-
-			 * The following condition tests for a line down the left:
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 */
-			if (boardState[0] == lastMove && boardState[3] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a line across the bottom:
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		| * | * | * |
-			 * 		-------------
-			 */
-			else if (boardState[7] == lastMove && boardState[8] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a diagonal line (bottom,left to top,right):
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 */
-			else if (boardState[2] == lastMove && boardState[4] == lastMove) {
-				return true;
-			}
-		case 7:
-			/*-
-			 * The following condition tests for a line across the bottom:
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		| * | * | * |
-			 * 		-------------
-			 */
-			if (boardState[6] == lastMove && boardState[8] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a line down the middle:
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 */
-			else if (boardState[1] == lastMove && boardState[7] == lastMove) {
-				return true;
-			}
-		case 8:
-			/*-
-			 * The following condition tests for a line across the bottom:
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		|   |   |   |
-			 * 		-------------
-			 * 		| * | * | * |
-			 * 		-------------
-			 */
-			if (boardState[6] == lastMove && boardState[7] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a line down the right side:
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 */
-			else if (boardState[2] == lastMove && boardState[5] == lastMove) {
-				return true;
-			}
-			/*-
-			 * The following condition tests for a diagonal:
-			 * 		-------------
-			 * 		| * |   |   |
-			 * 		-------------
-			 * 		|   | * |   |
-			 * 		-------------
-			 * 		|   |   | * |
-			 * 		-------------
-			 */
-			else if (boardState[0] == lastMove && boardState[4] == lastMove) {
-				return true;
-			}
-		default:
-			return false;
 		}
 
 	}
